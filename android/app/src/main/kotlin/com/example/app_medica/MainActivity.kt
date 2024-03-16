9739d5cef9bb4658f8200a6102939ebfcda3187d
Oscar Lopez <osdaloji@gmail.com>
2024-03-16 11:12:39 -0500

    feat: :sparkles: primer commit del proyecto!

---
+++ android/app/src/main/kotlin/com/example/app_medica/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_medica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Aplicación Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Text(
+          '¡Hola, mundo!',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
